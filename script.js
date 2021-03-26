143fdd1bff16c329ffe7edbea19ce2d38a00fb6f
unknown <admin@Chetuiwk267.chetu.com>
2021-03-26 17:08:43 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,8 @@
+// function PrintClock() {
+//   var d = new Date();
+//   document.body.innerHTML = "<h2>" + d.toTimeString() + "</h2>";
+// }
+
+let sum = (function (x, y) {
+  console.log("IIEF" + x * y);
+})(4, 7);
